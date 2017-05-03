900748cae791e2448d6f9542628e6bec2bd2669d
onserio12 <Onserio@gmail.com>
2017-05-03 16:24:37 +0300

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TestConsole
+{
+    public class Circle
+    {
+ 
+        // member variables
+        double _radius;
+  
+        public Circle(double radius)
+        {
+            _radius = radius;
+        
+        }
+
+        public double Diameter
+        {
+            get { return _radius*2; }
+        }
+
+        public double Circumference
+        {
+            get { return Math.PI * Diameter; }
+        }
+        public double Area
+        {
+            get { return Math.PI*Math.Pow(_radius, 2); }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Radius: {0}", _radius);
+            Console.WriteLine("Diameter: {0}", Diameter);
+            Console.WriteLine("Circumference: {0}", Circumference);
+            Console.WriteLine("Area: {0}", Area);
+        }
+    }
+
+    class ExecuteCircle
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(10);
+          
+            c.Display();
+            Console.ReadLine();
+        }
+    }
+}
